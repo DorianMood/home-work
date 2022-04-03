c73ea2ef2cadc3bdc805fb764543d7869d0e015e
DorianMood <nikita01051997@gmail.com>
2022-04-03 18:11:16 +0500

    08 done

--- 08/index.js
+++ 08/index.js
@@ -1,7 +1,33 @@
 function sum(a, b) {
-  return (BigInt(a) + BigInt(b)).toLocaleString("fullwide", {
-    useGrouping: false,
-  });
+  if (Number.isInteger(a) && Number.isInteger(b)) {
+    return (a + b).toString();
+  } else if (Number.isInteger(b)) {
+    b = b.toString();
+  } else if (Number.isInteger(a)) {
+    a = a.toString();
+  }
+
+  if (a.length > b.length) {
+    b = "0".repeat(a.length - b.length) + b;
+  } else if (a.length < b.length) {
+    a = "0".repeat(b.length - a.length) + a;
+  }
+
+  let result = "";
+  let carry = 0;
+
+  for (let i = a.length - 1; i >= 0; i--) {
+    let sum = a.charCodeAt(i) - 48 + (b.charCodeAt(i) - 48) + carry;
+    result += sum % 10;
+    carry = Math.floor(sum / 10);
+  }
+
+  if (carry) result += carry;
+  result = result.split("").reverse().join("");
+
+  return result;
 }
+
+sum(1, 1);
 
 module.exports = sum;
